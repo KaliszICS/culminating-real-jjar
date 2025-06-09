85605e80a85eec560f5560713c05efcafc5e495a
justin <justin.chu4@student.tdsb.on.ca>
2025-06-08 21:05:28 -0400

    moved the standings calculation to the standings class so future sportsleagues classes can use it

--- src/main/java/BasketballLeague.java
+++ src/main/java/BasketballLeague.java
@@ -1,80 +1,17 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class BasketballLeague extends League {
+    private Standings standings;
 
     public BasketballLeague() {
         super("Basketball");
+        this.standings = new Standings(teams, games);
     }
 
     @Override
     public void calculateStandings() {
-        //buble to merge
-        String[] teamsArray = teams.toArray(new String[0]);
-        mergeSort(teamsArray, 0, teamsArray.length - 1);
-        
-        teams.clear();
-        teams.addAll(Arrays.asList(teamsArray));
-        
-        String standings = "Basketball League Standings:\n";
-        for (int i = 0; i < teams.size(); i++) {
-            String team = teams.get(i);
-            standings += team + " - Wins: " + getTeamWins(team) +
-                    ", Points Scored: " + getTeamPointsScored(team) + "\n";
-        }
-        teamStandings = standings;
-    }
-
-    private void mergeSort(String[] arr, int left, int right) {
-        if (left < right) {
-            int mid = (left + right) / 2;
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-            merge(arr, left, mid, right);
-        }
-    }
-
-    private void merge(String[] arr, int left, int mid, int right) {
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-
-        String[] L = new String[n1];
-        String[] R = new String[n2];
-
-        for (int i = 0; i < n1; i++) {
-            L[i] = arr[left + i];
-        }
-        for (int j = 0; j < n2; j++) {
-            R[j] = arr[mid + 1 + j];
-        }
-
-        int i = 0, j = 0, k = left;
-        while (i < n1 && j < n2) {
-            int wins1 = getTeamWins(L[i]);
-            int wins2 = getTeamWins(R[j]);
-            
-            if (wins1 > wins2 || (wins1 == wins2 && 
-                getTeamPointsScored(L[i]) > getTeamPointsScored(R[j]))) {
-                arr[k] = L[i];
-                i++;
-            } else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
+        standings.calculateStandings();
+        teamStandings = standings.getStandings();
     }
 
     @Override
@@ -96,36 +33,44 @@ public class BasketballLeague extends League {
 
     @Override
     public void updateTeamStats(Team team) {
-
-    }
-
-    private int getTeamWins(String teamName) {
         int wins = 0;
-        for (int i = 0; i < games.size(); i++) {
-            Game game = games.get(i);
-            if (game.getTeam1().getTeamName().equals(teamName) && game.getTeam1Score() > game.getTeam2Score()) {
-                wins++;
-            } 
-            
-            else if (game.getTeam2().getTeamName().equals(teamName) && game.getTeam2Score() > game.getTeam1Score()) {
-                wins++;
-            }
-        }
-        return wins;
-    }
+        int losses = 0;
+        int pointsScored = 0;
+        int pointsAgainst = 0;
 
-    private int getTeamPointsScored(String teamName) {
-        int points = 0;
         for (int i = 0; i < games.size(); i++) {
             Game game = games.get(i);
-            if (game.getTeam1().getTeamName().equals(teamName)) {
-                points += game.getTeam1Score();
+            if (game.getTeam1().getTeamName().equals(team.getTeamName())) {
+                pointsScored += game.getTeam1Score();
+                pointsAgainst += game.getTeam2Score();
+                if (game.getTeam1Score() > game.getTeam2Score()) {
+                    wins++;
+                } 
+                
+                else if (game.getTeam1Score() < game.getTeam2Score()) {
+                    losses++;
+                }
             } 
             
-            else if (game.getTeam2().getTeamName().equals(teamName)) {
-                points += game.getTeam2Score();
+            else if (game.getTeam2().getTeamName().equals(team.getTeamName())) {
+                pointsScored += game.getTeam2Score();
+                pointsAgainst += game.getTeam1Score();
+                if (game.getTeam2Score() > game.getTeam1Score()) {
+                    wins++;
+                } 
+                
+                else if (game.getTeam2Score() < game.getTeam1Score()) {
+                    losses++;
+                }
             }
         }
-        return points;
+
+
+        System.out.println("\nTeam Statistics for " + team.getTeamName() + ":");
+        System.out.println("Wins: " + wins);
+        System.out.println("Losses: " + losses);
+        System.out.println("Points Scored: " + pointsScored);
+        System.out.println("Points Against: " + pointsAgainst);
+        System.out.println("Point Differential: " + (pointsScored - pointsAgainst));
     }
 } 
--- src/main/java/Standings.java
+++ src/main/java/Standings.java
@@ -1,24 +1,116 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Standings {
-    private HashMap<Team, Integer> rankings;
+    private ArrayList<String> teams;
+    private ArrayList<Game> games;
+    private String standings;
 
-    public Standings(HashMap<Team, Integer> rankings) {
-        this.rankings = rankings;
+    public Standings(ArrayList<String> teams, ArrayList<Game> games) {
+        this.teams = teams;
+        this.games = games;
+        this.standings = "";
     }
 
-    public void calculateRankings() {
-        for(Team team : rankings.keySet()) {
-            int points = rankings.get(team);
-            System.out.println("Team: " + team.getTeamName() + ", Points: " + points);
+    public void calculateStandings() {
+        String[] teamsArray = teams.toArray(new String[0]);
+        mergeSort(teamsArray, 0, teamsArray.length - 1);
+        
+        teams.clear();
+        teams.addAll(Arrays.asList(teamsArray));
+        
+        standings = "League Standings:\n";
+        for (int i = 0; i < teams.size(); i++) {
+            String team = teams.get(i);
+            standings += team + " - Wins: " + getTeamWins(team) +
+                    ", Points Scored: " + getTeamPointsScored(team) + "\n";
         }
     }
 
-    public ArrayList<String> displayStandings() {
-        ArrayList<String> standingList = new ArrayList<>();
-        for (Team team : rankings.keySet()) {
-            int points = rankings.get(team);
-            standingList.add("Team: " + team.getTeamName() + ", Points: " + points);
+    private void mergeSort(String[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
         }
-        return standingList;
+    }
+
+    private void merge(String[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        String[] L = new String[n1];
+        String[] R = new String[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            int wins1 = getTeamWins(L[i]);
+            int wins2 = getTeamWins(R[j]);
+            
+            if (wins1 > wins2 || (wins1 == wins2 &&  getTeamPointsScored(L[i]) > getTeamPointsScored(R[j]))) {
+                arr[k] = L[i];
+                i++;
+            } 
+            
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    private int getTeamWins(String teamName) {
+        int wins = 0;
+        for (int i = 0; i < games.size(); i++) {
+            Game game = games.get(i);
+            if (game.getTeam1().getTeamName().equals(teamName) && game.getTeam1Score() > game.getTeam2Score()) {
+                wins++;
+            } 
+
+            else if (game.getTeam2().getTeamName().equals(teamName) && game.getTeam2Score() > game.getTeam1Score()) {
+                wins++;
+            }
+        }
+        return wins;
+    }
+
+    private int getTeamPointsScored(String teamName) {
+        int points = 0;
+        for (int i = 0; i < games.size(); i++) {
+            Game game = games.get(i);
+            if (game.getTeam1().getTeamName().equals(teamName)) {
+                points += game.getTeam1Score();
+            } 
+            
+            else if (game.getTeam2().getTeamName().equals(teamName)) {
+                points += game.getTeam2Score();
+            }
+        }
+        return points;
+    }
+
+    public String getStandings() {
+        return standings;
     }
 }
